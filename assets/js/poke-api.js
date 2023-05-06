65237f300270db2411d3789e5bfde8aa1eafe57a
Vinicius de Souza Fonseca <vinicius.fonseca@fgv.br>
2023-05-06 01:36:20 -0300

    modal

--- assets/js/poke-api.js
+++ assets/js/poke-api.js
@@ -1,35 +1,69 @@
-
-const pokeApi = {}
+const pokeApi = {};
 
 function convertPokeApiDetailToPokemon(pokeDetail) {
-    const pokemon = new Pokemon()
-    pokemon.number = pokeDetail.id
-    pokemon.name = pokeDetail.name
+  const pokemon = new Pokemon();
+  pokemon.number = pokeDetail.id;
+  pokemon.name = pokeDetail.name;
 
-    const types = pokeDetail.types.map((typeSlot) => typeSlot.type.name)
-    const [type] = types
+  const types = pokeDetail.types.map((typeSlot) => typeSlot.type.name);
+  const [type] = types;
 
-    pokemon.types = types
-    pokemon.type = type
+  pokemon.types = types;
+  pokemon.type = type;
 
-    pokemon.photo = pokeDetail.sprites.other.dream_world.front_default
+  pokemon.photo = pokeDetail.sprites.other.dream_world.front_default;
 
-    return pokemon
+  return pokemon;
 }
 
 pokeApi.getPokemonDetail = (pokemon) => {
-    return fetch(pokemon.url)
-        .then((response) => response.json())
-        .then(convertPokeApiDetailToPokemon)
-}
+  return fetch(pokemon.url)
+    .then((response) => response.json())
+    .then(convertPokeApiDetailToPokemon);
+};
 
 pokeApi.getPokemons = (offset = 0, limit = 5) => {
-    const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`
+  const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`;
 
-    return fetch(url)
-        .then((response) => response.json())
-        .then((jsonBody) => jsonBody.results)
-        .then((pokemons) => pokemons.map(pokeApi.getPokemonDetail))
-        .then((detailRequests) => Promise.all(detailRequests))
-        .then((pokemonsDetails) => pokemonsDetails)
+  return fetch(url)
+    .then((response) => response.json())
+    .then((jsonBody) => jsonBody.results)
+    .then((pokemons) => pokemons.map(pokeApi.getPokemonDetail))
+    .then((detailRequests) => Promise.all(detailRequests))
+    .then((pokemonsDetails) => pokemonsDetails);
+};
+
+pokeApi.getPokemonById = (id) => {
+  const url = `https://pokeapi.co/api/v2/pokemon/${id}`;
+
+  return fetch(url)
+    .then((response) => response.json())
+    .then((jsonBody) => convertPokemonByIdToPokemon(jsonBody))
+    .then((pokemonsDetails) => pokemonsDetails);
+};
+
+function convertPokemonByIdToPokemon(pokemonDetails) {
+  const pokemon = new Pokemon();
+  pokemon.name = pokemonDetails.name;
+  pokemon.number = pokemonDetails.id;
+
+  const types = pokemonDetails.types.map((types) => types.type.name);
+  const [type] = types;
+
+  pokemon.mainType = type;
+  pokemon.types = types;
+  pokemon.photo = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${pokemonDetails.id}.png`;
+  pokemon.baseExperience = pokemonDetails.base_experience;
+  pokemon.height = pokemonDetails.height;
+  pokemon.weight = pokemonDetails.weight;
+
+  const stats = pokemonDetails.stats.map((stats) => {
+    return {
+      name: stats.stat.name,
+      number: stats.base_stat,
+    };
+  });
+
+  pokemon.stats = stats;
+  return pokemon;
 }
